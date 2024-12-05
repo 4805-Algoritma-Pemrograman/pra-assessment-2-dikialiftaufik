134f1ad209517df79b1ed6f33aad35efd5cca323
dikialiftaufik <dikialift13@gmail.com>
2024-12-05 12:22:11 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -9,7 +9,22 @@ public class Gaji{
         int jamLembur = input.nextInt();
 
         /* Pekerjaan anda mulai dari sini */
-
+        if (masaKerja < 5) {
+            int gajiPokok = 5000000;
+            int uangLembur = jamLembur * 40000;
+            int totalGaji = gajiPokok + uangLembur;
+            System.out.println(totalGaji);
+        } else if (masaKerja >= 5 && masaKerja <= 10) {
+            int gajiPokok = 7500000;
+            int uangLembur = jamLembur * 50000;
+            int totalGaji = gajiPokok + uangLembur;
+            System.out.println(totalGaji);
+        } else {
+            int gajiPokok = 10000000;
+            int uangLembur = jamLembur * 75000;
+            int totalGaji = gajiPokok + uangLembur;
+            System.out.println(totalGaji);
+        }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Gaji.class.getDeclaredMethods().length);
