a00620ea54036366a801125bb6617fc5b71bd78f
dikialiftaufik <dikialift13@gmail.com>
2024-12-05 12:27:49 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -9,22 +9,35 @@ public class Gaji{
         int jamLembur = input.nextInt();
 
         /* Pekerjaan anda mulai dari sini */
-        if (masaKerja < 5) {
-            int gajiPokok = 5000000;
-            int uangLembur = jamLembur * 40000;
-            int totalGaji = gajiPokok + uangLembur;
-            System.out.println(totalGaji);
-        } else if (masaKerja >= 5 && masaKerja <= 10) {
-            int gajiPokok = 7500000;
-            int uangLembur = jamLembur * 50000;
-            int totalGaji = gajiPokok + uangLembur;
-            System.out.println(totalGaji);
-        } else {
-            int gajiPokok = 10000000;
-            int uangLembur = jamLembur * 75000;
-            int totalGaji = gajiPokok + uangLembur;
-            System.out.println(totalGaji);
+        int gajiPokok = hitungGaji();
+        int uangLembur = hitungUangLembur(jamLembur);
+        int totalGaji = gajiPokok + uangLembur;
+
+        // Function untuk menghitung gaji pokok berdasarkan masa kerja
+        public static int hitungGaji() {
+            if (masaKerja < 5) {
+                return 5000000;
+            } else if (masaKerja <= 10) {
+                return 7500000;
+            } else {
+                return 10000000;
+            }
         }
+
+        // Function untuk menghitung uang lembur berdasarkan masa kerja dan jam lembur
+        public static int hitungUangLembur(int jamLembur) {
+            int honorPerJam;
+            if (masaKerja < 5) {
+                honorPerJam = 40000;
+            } else if (masaKerja <= 10) {
+                honorPerJam = 50000;
+            } else {
+                honorPerJam = 75000;
+            }
+            return jamLembur * honorPerJam;
+        }
+
+        System.out.println("Total Gaji: " + totalGaji);
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Gaji.class.getDeclaredMethods().length);
